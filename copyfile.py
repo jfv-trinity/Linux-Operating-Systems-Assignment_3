b71cc4871ff0a486ee4cd1a2306fbe309707d919
jfv-Trinity <Shadehunter@hotmail.com>
2020-08-06 16:08:06 -0400

    No CVS import allowed. Simple command is fine also.

--- copyfile.py
+++ copyfile.py
@@ -1,10 +1,7 @@
 #!/usr/bin/env python3
 
 source = input("Please type the origin file:\n")
-print(f'you entered: {source}')
-
 destination = input("Please type file Destination:\n")
-print(f'you entered: {destination}')
 
 with open(source) as f:
     with open(destination, "w") as f1:
--- payroll.py
+++ payroll.py
@@ -3,18 +3,9 @@
 payroll.py
 Joseph Volpe
 301118010
-reads data.txt file and outputs each line on the data.txt file using a CVS translater.
+reads data.txt file and outputs each line on the data.txt file.
 """
-import csv
 
-with open('data.txt') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    line_count = 0
-    for row in csv_reader:
-        if line_count == 0:
-            print(f'{"Employee Number, Employee Name, Hours Worked".join(row)}')
-            line_count += 1
-        else:
-            print(f'\t{row[0]}  {row[1]} {row[2]} {row[3]}')
-            line_count += 1
-    print(f'Processed {line_count} lines.')
+f = open("data.txt", "r")
+print(f.read())
+
